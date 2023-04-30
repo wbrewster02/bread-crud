ded6581067dd0fd4380b5e8b64b07f7f0650ede6
wbrewster02 <wbrewster02@gmail.com>
2023-04-30 11:03:06 -0400

    controllers, models, .gitignore, etc added

---
+++ controllers/bread.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const Bread = require('../models/bread')
+
+//get all the bread
+router.get('/', (req, res) => {
+    res.send(Bread)
+})
+
+//get a specific bread (by using the index of of the array)
+router.get('/:index', (req, res) => {
+    const { index } = req.params
+    res.send(Bread[index])
+})
+module.exports = router
