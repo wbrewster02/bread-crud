283bef1a7b4ac314fb1fdd8c01c1d377efd3e678
wbrewster02 <wbrewster02@gmail.com>
2023-05-16 18:45:28 -0400

    cascade deletion on baker deleted

--- controllers/baker.js
+++ controllers/baker.js
@@ -14,6 +14,12 @@ router.get('/:id', async(req, res) =>{
     })
 })
 
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+  await Baker.findByIdAndDelete(id)
+  res.status(303).redirect('/breads')
+})
+
 router.get('/data/seed', async (req, res) => {
     const data = [
         {
--- models/baker.js
+++ models/baker.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Bread = require('./bread')
 
 const bakerSchema = new mongoose.Schema({
     name: {
@@ -20,8 +21,12 @@ const bakerSchema = new mongoose.Schema({
     bio: String
 }, {
     toJSON: { virtuals: true}
-}
-)
+})
+
+bakerSchema.post('findOneAndDelete', async function() {
+    await Bread.deleteMany({ baker: this._conditions._id})
+})
+
 bakerSchema.virtual('breads', {
     ref: 'Bread',
     localField: '_id',
